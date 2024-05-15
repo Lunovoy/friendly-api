764eb9f1a7c6af3b63074b6f3c17e14eac8c7082
Lunovoy <anuilov1991@mail.ru>
2024-05-15 08:55:13 +0300

    update tables

--- db/migrations/000009_tg_chat_table.up.sql
+++ db/migrations/000009_tg_chat_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "tg_chat" (
     "id" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    "chat_id" bigint not null,
+    "chat_id" bigint unique not null,
     "user_id" UUID unique not null,
     FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE
 );
