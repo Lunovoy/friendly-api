9526e677e40db3c2082577584672e0ee61a7e4a7
Lunovoy <anuilov1991@mail.ru>
2024-05-15 08:57:20 +0300

    fix error in migrations

--- db/migrations/000010_reminder_table.up.sql
+++ db/migrations/000010_reminder_table.up.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS "reminder" {
+CREATE TABLE IF NOT EXISTS "reminder" (
   "id" UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
   "minutes_until_event" integer not null,
   "event_id" UUID not null,
   "user_id" UUID not null,
   FOREIGN KEY ("event_id") REFERENCES "event" ("id") ON DELETE CASCADE,
   FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE
-}
+);
