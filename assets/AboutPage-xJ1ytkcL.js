1f4201183af1fef7d82aa7201e9280dd585ea9cc
Lunovoy <anuilov1991@mail.ru>
2024-05-26 22:11:04 +0300

    update static files

---
+++ assets/AboutPage-xJ1ytkcL.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CvvDs7yX.js";import{u as r}from"./useTranslation-DdVfk-uP.js";const e=()=>{const{t}=r("about");return o.jsx("div",{children:t("О сайте")})};export{e as default};
