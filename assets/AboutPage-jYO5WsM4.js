c4f9670a97cf57f518e2f4159f7e2fe858722dfd
Lunovoy <anuilov1991@mail.ru>
2024-05-27 00:12:18 +0300

    update static

---
+++ assets/AboutPage-jYO5WsM4.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-BiqYdbNS.js";import{u as r}from"./useTranslation-z379OQBV.js";const e=()=>{const{t}=r("about");return o.jsx("div",{children:t("О сайте")})};export{e as default};
