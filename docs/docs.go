18eee7ad79b1d8d777e6377b9fd147e6df2b2aed
Lunovoy <anuilov1991@mail.ru>
2024-05-28 17:49:50 +0300

    update swagger

--- docs/docs.go
+++ docs/docs.go
@@ -3830,7 +3830,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "80.78.243.140",
+	Host:             "http://89.111.170.101",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Friendly app API",
